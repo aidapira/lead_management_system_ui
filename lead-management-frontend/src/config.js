785d01508166d7339b72f2ce7d6b90fdd6a24cca
Aida <aidapirah@gmail.com>
2024-12-02 14:56:15 -0800

    Fix extra /:

--- lead-management-frontend/src/config.js
+++ lead-management-frontend/src/config.js
@@ -1,6 +1,6 @@
 const API_BASE_URL =
   process.env.NODE_ENV === "production"
-    ? "https://lead-management-system-service.onrender.com/"
+    ? "https://lead-management-system-service.onrender.com"
     : "http://localhost:5001";
 
 export default API_BASE_URL;
